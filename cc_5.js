f478a8dd9f20e84233cfa5cf00d91f448e06ee54
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-05-22 18:31:29 -0400

    Instantiate Manager objects and calculate annual salaries with bonuses.

--- cc_5.js
+++ cc_5.js
@@ -28,8 +28,9 @@ class Employee {
 //Add a department property and override the annual salary calculation method to include a 15% bonus. 
 class Manager extends Employee {
   constructor(employeeName, employeeMonthlySalary, department) {
-    super(employeeName, employeeMonthlySalary)
-    this.department = department
+    super(employeeName, employeeMonthlySalary);
+    this.department = department;
+    console.log ("Department: " + this.department);
   }
 
   annualSalaryCalc() {
@@ -37,10 +38,15 @@ class Manager extends Employee {
     let salaryBonus = baseAnnualSalary * managerSalaryBonusMultiplier;
     let totalAnnualSalary = baseAnnualSalary + salaryBonus;
         //Log the amount of the bonus and the total annual salary including the bonus.
-    console.log("Bonus: " + USDconvert(salaryBonus) + "\nTotal Annual Salary: " + USDconvert(totalAnnualSalary));
+    console.log("Yearly Bonus: " + USDconvert(salaryBonus) + "\nTotal Annual Salary: " + USDconvert(totalAnnualSalary));
   }
 }
-testEmployee = new Employee ("Alan", 12);
-testManager = new Manager ("James",1203038, "Sales")
-testManager.annualSalaryCalc()
-testEmployee.annualSalaryCalc()
+
+//Create two instances of the Manager class using the provided test data. Calculate and log their annual salaries, including bonuses.
+//Manager 1: Bob Ross works in the Digital Marketing department and earns $6600.00 a month.
+let manager1 = new Manager("Bob Ross", 6600, "Digital Marketing");
+manager1.annualSalaryCalc();
+
+//Manager 2: Debbie Little works in the Finance department and earns $7205.00 a month.
+let manager2 = new Manager("Debbie Little", 7205, "Finance");
+manager2.annualSalaryCalc();
